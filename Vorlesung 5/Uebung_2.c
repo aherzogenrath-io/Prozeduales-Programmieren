81928a04fa61733e54293d40562efe420fe58a50
aherzogenrath-io <aherzogenrath@infonline.de>
2024-10-31 11:24:07 +0100

    Number guessing game
    Random number is generated by the system time.
    The player has to guess the number and gets told if the guess is too high or too low. The player has three lives.
    The game can be repeated as often as wanted

--- Vorlesung 5/Uebung_2.c
+++ Vorlesung 5/Uebung_2.c
@@ -1,6 +1,51 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
 
 int main(void)
 {
 
+    int input;
+    char OK;
+    bool again = true;
+    time_t sec;
+    int zz;
+
+    do
+    // Schleife für das Spiel, so lange der user j/J eingibt
+        {
+            sec = time (NULL); //Zeit in Sekunden ermitteln
+            srand((int)sec); // Initialisierung
+            zz = (rand() % 15) + 1;
+            for (int i = 3; i > 0; i--)
+            //Spiel mit 3 Leben und Prüfung, ob die Zahl richtig/zu hoch/zu niedrig ist
+            {
+                printf("Ihre Zahl:\n");
+                scanf("%d", &input);
+                if (input == zz) // Case unwahrscheinlich --> letzter Case
+                {
+                    printf("Correct!\n");
+                    break;
+                } else if (input > zz)
+                {
+                    printf("Your number too hight");
+                } else
+                {
+                    printf("Your number too low");
+                }
+                    printf("\nLives left: %d\n", i - 1);
+
+            }
+            printf("The number was: %d\n", zz);
+            printf("Try again? (j/J; n/N)\n");
+            scanf(" %c", &OK);
+            switch(OK) {
+                case 'j': ;
+                case 'J': break;
+                case 'n': ;
+                case 'N': again = false;
+            }
+        } while (again);
+        return 0;
 }
